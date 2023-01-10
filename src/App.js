d79fe1b24d95e4096fb9b017a129b22699331ab4
Federico Carrillo <carrillofa1988@gmail.com>
2023-01-10 20:02:38 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 import NavBar from "./components/navBar/NavBar";
+import Form from "./components/form/Form";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/category/:id" element={<ItemListContainer />} />
         <Route path="/category" element={<ItemListContainer />} />
         <Route path="/item/:id" element={<ItemDetailContainer />} />
+        <Route path="/checkout" element={<Form />} />
         <Route path="/cart" element={<h1>Aca va el carrito</h1>} />
         <Route path="*" element={<h1>Esta URL no existe</h1>} />
       </Routes>
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+
+const Form = () => {
+  const [userData, setUserData] = useState({ name: "", lastName: "" });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  console.log(userData.name, userData.lastName);
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingrese su Nombre"
+          name="name"
+          onChange={(event) =>
+            setUserData({ ...userData, name: event.target.value })
+          }
+          value={userData.name}
+        />
+        <input
+          type="text"
+          placeholder="Ingrese su Apellido"
+          name="lastName"
+          onChange={(event) =>
+            setUserData({ ...userData, lastName: event.target.value })
+          }
+          value={userData.lastName}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
